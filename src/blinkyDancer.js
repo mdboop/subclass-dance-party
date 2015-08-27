7636cf4ad8717fb42c47a8fa8a4d7580a20a26bc
mdboop <mdeanbrooks@gmail.com>
2015-08-26 20:38:35 -0700

    Update animations and asset images

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,6 +1,7 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
   makeDancer.apply(this, arguments);
   this.timeBetweenSteps = timeBetweenSteps;
+  this.$node.addClass("blinkyDancer");
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   //debugger;
