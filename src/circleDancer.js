200595b1f54754e4cce55653252db4d7213694c8
mdboop <mdeanbrooks@gmail.com>
2015-08-26 21:41:22 -0700

    Fix circular animations and add gifs

--- src/circleDancer.js
+++ src/circleDancer.js
@@ -2,6 +2,7 @@ var makeCircleDancer = function(top, left, timeBetweenSteps){
   makeDancer.apply(this, arguments);
   this.timeBetweenSteps = timeBetweenSteps;
   this.$node.addClass("circleDancer");
+  this.$node.append('<img class="circleDancer" src=assets/skeleton.gif>')
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   //debugger;
